09f4c44a53c1d4f5453a122d6fcfa08c649b277f
えむけー <makoto_16n1200102@nnn.ed.jp>
2018-01-17 08:49:04 +0900

    #4 モデル定義をclass構文で書き直しました

--- models/user.js
+++ models/user.js
@@ -1,37 +1,43 @@
 'use strict'
+
 const loader = require('./sequelize-loader')
 const Sequelize = loader.Sequelize
+const sequelize = loader.database
 const bcrypt = require('bcrypt')
 
-const User = loader.database.define('users', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  password_hash: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
+class User extends Sequelize.Model {
+  verifyPassword(password) {
+    return bcrypt.compare(password, this.password_hash)
   }
-}, {
+}
+
+User.init(
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  }, {
     freezeTableName: true,
     underscored: true,
-    timestamps: true
-  })
-
-User.prototype.verifyPassword = function(password) {
-  console.log(`password_hash=[${this.password_hash}]`)
-  return bcrypt.compare(password, this.password_hash)
-}
+    timestamps: true,
+    sequelize
+  }
+)
 
 module.exports = User
 
